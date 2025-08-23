ec2be105debc46e25956a8426ac0d99b43955eda
lu-lui <luisaneutzlingpassos@gmail.com>
2025-08-23 19:08:37 -0300

    Update ex2_1.js

--- cap02/js/ex2_1.js
+++ cap02/js/ex2_1.js
@@ -1,6 +1,7 @@
 // cria referência ao form e ao elemento h3 (onde será exibida a resposta)
 const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
+//cria a referência com um único comando: const nome = document.querySelector("form").inNome.value
 
 // cria um "ouvinte" de evento, acionado quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
