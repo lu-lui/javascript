8a6be8b2524bf21507002e279643e7bddc1ffdf6
lu-lui <luisaneutzlingpassos@gmail.com>
2025-09-04 22:50:37 -0300

    Update resp4_b.js

---
+++ cap04/js/resp4_b.js
@@ -0,0 +1,22 @@
+// Elaborar um programa que diga se o condutor levou multa ou não. Velocidade <= que a permitida: "Sem multa", valocidade até 20% a mais que o permitido: "Multa Leve". Superior a 20%: "Multa Grave"
+
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const permitida = Number(frm.inPermitida.value)
+    const condutor = Number(frm.inCondutor.value)
+
+    if (condutor <= permitida) {
+        resp.innerText = "Situação: Sem multa"
+        resp.style.color = "darkgreen"
+    } else if (condutor <= permitida * 1.2) {
+        resp.innerText = "Situação: Multa Leve"
+        resp.style.color = "orange"
+    } else {
+        resp.innerText = "Situação: Multa Grave"
+        resp.style.color = "red"
+    }
+})
